1725e3826b8dfac985428eb22f50d372dab790c0
kim <perfumeplaylist@gmail.com>
2024-11-24 01:45:37 +0900

    fix: 사용하지 않는 함수 제거

--- src/screens/viewLetter/viewLetter.tsx
+++ src/screens/viewLetter/viewLetter.tsx
@@ -1,11 +1,6 @@
 import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native';
-import calculateDaysUntilChristmas from 'src/shared/utils/calculateDaysUntilChristmas';
 
 const ViewLetterScreen = () => {
-  const diffInDays = calculateDaysUntilChristmas();
-  console.log(diffInDays);
-  // 이미지 받아오기
-
   return (
     <SafeAreaView>
       <View className="px-[18px]">
