7be9e1c0915138429f61ce8197ef5384b46c8c69
JJHyeok13 <jjinh99@naver.com>
2024-11-23 16:55:10 +0900

    project setting

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    // Native Wind 설정
+    ['nativewind/babel'],
+    // Alias 설정
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        alias: {
+          '@app': './src/app',
+          '@routes': './src/app/routes',
+          '@widgets': './src/widgets',
+          '@screens': './src/screens',
+          '@assets': './src/shared/assets',
+          '@axios': './src/shared/axios',
+          '@zustand': './src/shared/zustand',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/screens/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, View } from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <SafeAreaView>
+      <View />
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
