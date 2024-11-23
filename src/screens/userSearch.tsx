6889e7977371f3a23be15ae0d020f451aa60b47c
kim <perfumeplaylist@gmail.com>
2024-11-23 23:19:31 +0900

    feat: userSearch 페이지 구현

---
+++ src/screens/userSearch.tsx
@@ -0,0 +1,34 @@
+import { Pressable, SafeAreaView, Text, TextInput, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import useDebounce from 'src/shared/hooks/useDebounce';
+
+const UserSearchScreen = () => {
+  const [keyword, setKeyword] = useState('');
+  const debounceValue = useDebounce<string>(keyword, 300);
+
+  useEffect(() => {
+    if (debounceValue) {
+      // 네트워크 요청
+    }
+  }, [debounceValue]);
+
+  return (
+    <SafeAreaView>
+      <View className="px-[18px]">
+        <View className="flex-row relative">
+          <TextInput
+            value={keyword}
+            onChangeText={(text: string) => setKeyword(text)}
+            className="border-2 border-gray100 bg-white border-r-2 px-5 py-2.5 flex-1"
+            placeholder="닉네임으로 친구를 찾아보세요"
+          />
+          <Pressable onPress={() => setKeyword('')} className="absolute right-2 top-3">
+            <Text>초기화</Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default UserSearchScreen;
